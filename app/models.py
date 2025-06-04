5fb4104c59f7ee1b897bb6a231b02463e2a511ad
TechnologicalJerry <cserajatverma@gmail.com>
2025-06-04 21:37:11 +0530

    Delete models.py

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column, String, Boolean
-from app.database import Base
-
-class User(Base):
-    __tablename__ = "users"
-    phone = Column(String, primary_key=True, index=True)
-    is_verified = Column(Boolean, default=False)
